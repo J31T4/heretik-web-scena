0671212c8f09d8879604c00b52a0004e6b02b753
Matěj Kuchler <matej.kuchler40@github.com>
2026-08-02 15:20:54 +0200

    Scena: divadelní koncept - Představení začíná (marquee, timeline, fire-text, dějství)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-// Astro konfigurace
-// https://astro.build/config
 export default defineConfig({
   site: 'https://j31t4.github.io',
-  base: '/heretik-web',
+  base: '/heretik-web-scena',
   trailingSlash: 'never',
 });
